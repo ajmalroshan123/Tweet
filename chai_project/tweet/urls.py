21106110388860e41a95b69439cf2aec0ad1a388
ajmalroshan123 <ajmalroshan2002@gmail.com>
2024-12-04 21:05:59 +0530

    feat: tweet mvp

--- chai_project/tweet/urls.py
+++ chai_project/tweet/urls.py
@@ -2,7 +2,11 @@ from . import views
 from django.urls import path        
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.tweet_list, name='tweet_list'),
+    path('create/', views.create_tweet, name='create_tweet'),
+    path('<int:tweet_id>/edit/', views.edit_tweet, name='edit_tweet'),
+    path('<int:tweet_id>/delete/', views.delete_tweet, name='delete_tweet'),
+    path('register/', views.register, name='registration')
 ]
 
 
